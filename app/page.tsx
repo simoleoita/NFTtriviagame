2503ae1a4d40116df7c5eaea33ec1d4c136c097a
simoleoita <49583351+simoleoita@users.noreply.github.com>
2023-11-19 15:34:01 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Chat() {
       <div className="container mx-auto px-10 md:px-28">
         <h1 className="text-4xl font-bold text-center pt-20 pb-6"> Hello! Play Trivia with a Crypto-Enabled GPT to win an NFT </h1>
         <h2 className="text-md md:text-xl font-light text-center pb-8 px-8 lg:px-26">Start the conversation below, say Hi - Game on!</h2>
-        <h3 className="text-md md:text-xl font-light text-center pb-12 px-6 lg:px-26">Allow a few seconds to the system to start</h3>
+        <h3 className="text-md md:text-xl font-light text-center pb-12 px-6 lg:px-18">Allow a few seconds to the system to start</h3>
 
         <p className="text-xs font-extralight text-gray-500 text-center xs:px-24">
           This demo was built with just Next.js, OpenAI's API, and{' '}
